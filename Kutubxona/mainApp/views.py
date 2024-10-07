d63f814430aa2830ac7fab649e5905816124e63d
Otabek Tursunov <otabecktursunov@gmail.com>
2024-10-07 11:55:42 +0500

    just

--- Kutubxona/mainApp/views.py
+++ Kutubxona/mainApp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from django.utils import timezone
+from .models import *
+
+
+def home(request):
+    return HttpResponse("<h1>Home Page</h1>")
+
+
+def home_page(request):
+    data = {
+        'now_time': timezone.now(),
+        'students': Talaba.objects.all()
+    }
+    return render(request, 'home.html', data)
+
+
+def bitiruvchilar(request):
+    context = {
+        'students': Talaba.objects.filter(kurs=4)
+    }
+    return render(request, 'bitiruvchilar.html', context)
+
+
+def student_info(request, student_id):
+    # student = Talaba.objects.get(id=student_id)
+    student = get_object_or_404(Talaba, id=student_id)
+    context = {
+        'student': student
+    }
+    return render(request, 'student_info.html', context)
